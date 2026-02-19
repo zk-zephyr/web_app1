e22ccf34ce131f5a580b6a746d7bb141a4e0fd42
zk-zephyr <zofia.kaminski@gmail.com>
2026-02-18 21:02:51 -0600

    New project

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILE_PATH = r".\todo_app\todos.txt"
+
+def get_todos(filepath=FILE_PATH): #make the parameter/argument default
+    """Read the text file and return the list of todos""" #this is a doc string, used for documentation
+    #if someone runs a help function, this will show up such as print(help(get_todos))
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILE_PATH): #order matters and it's better to have the nondefault parameter
+    #first. So we switched it so that todos_arg is first.
+    """Write todos item list to a text file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+
+    print("Hello from the function")
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,34 @@
+#to run use: streamlit run .\todo_app\web.py
+
+import streamlit as st
+import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"] +"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    #Trodo is a local variable
+    #all the variables are arbitrary. todos is the list, but index and t/odo can be anything
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="Enter a todo:",
+              placeholder= "Add new todo",
+              on_change=add_todo, key='new_todo')
+
+#st.session_state
+#session state is used to monitor the variables while coding but
+#not something you need on the final app
